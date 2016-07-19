c55e997a74e6fb9430927368e4040fe661130b4b
Eduardo de Leon <edeleon4@mit.edu>
2016-07-19 15:39:39 -0400

    Added v1 of target balls. Dealing with relative angle corner cases

---
+++ targets.py
@@ -0,0 +1,37 @@
+def normFunc(items):
+		item_sum = sum(items)
+		return lambda x : float(x)/item_sum
+
+def get_hit_pos(pocket, target):
+		rad = target.rad
+		diam = 2*rad
+		# distance between the balls scaling delta to oen ball 			  # hypotenuse along the impact path
+		delta_x = pocket.x - target.x
+		delta_y = pocket.y - target.y
+		norm = normFunc([delta_x,delta_y])
+		x_shift = norm(delta_x)*diam
+		y_shift =  norm(delta_y)*diam
+	
+		hit_pos_x = target.x + x_shift
+		hit_pos_y = target.y + y_shift
+		# Hacky way of dealing with relative angles
+		# gaurantees the resulting ball is further from the pocket
+
+		if not (hit_pos_x - pocket.x) > (target.x - pocket.x):
+				hit_pos_x = target.x - x_shift
+						
+		if not (hit_pos_y - pocket.y) > (target.y - pocket.y):
+				hit_pos_y = target.y - y_shift
+		ans = [hit_pos_x, hit_pos_y]
+		
+		return ans
+
+class Ball:
+		def __init__(self,x,y):
+				self.x = x
+				self.y = y
+				self.rad = 2
+		
+pocket = Ball(2,4) 
+target = Ball(-3,8) 
+get_hit_pos(pocket,target)
