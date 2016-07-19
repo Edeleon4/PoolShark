429011cbc87cfa8be123579b087a66a9071bf9b8
Ilya Matiach <imatiach@yahoo.com>
2016-07-19 18:06:31 -0400

    Flask web project part 2
    Flask web project part 2

---
+++ Python279/Scripts/dateadd-script.py
@@ -0,0 +1,10 @@
+#!C:\Python279\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'dateutils==0.6.6','console_scripts','dateadd'
+__requires__ = 'dateutils==0.6.6'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('dateutils==0.6.6', 'console_scripts', 'dateadd')()
+    )
---
+++ Python279/Scripts/skivi-script.py
@@ -0,0 +1,10 @@
+#!C:\Python279\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'scikit-image==0.11.3','console_scripts','skivi'
+__requires__ = 'scikit-image==0.11.3'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('scikit-image==0.11.3', 'console_scripts', 'skivi')()
+    )
