0b842f94775c983bbefea73a4bb1237a9e11f81f
Ratan <ratan.r.sur@gmail.com>
2016-07-19 13:00:33 -0400

    classy balls

---
+++ balls.py
@@ -0,0 +1,37 @@
+class Ball:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class ObjectBall(Ball):
+    pass
+
+class One(ObjectBall):
+    pass
+
+class Two(ObjectBall):
+    pass
+
+class Three(ObjectBall):
+    pass
+
+class Four(ObjectBall):
+    pass
+
+class Five(ObjectBall):
+    pass
+
+class Six(ObjectBall):
+    pass
+
+class Seven(ObjectBall):
+    pass
+
+class Eight(ObjectBall):
+    pass
+
+class Nine(ObjectBall):
+    pass
+
+class CueBall(Ball):
+    pass
