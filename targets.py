e9de616816d85774befba8ceabda22a31455ea24
Eduardo de Leon <edeleon4@mit.edu>
2016-07-20 13:46:08 -0700

    resolved target hit point bug. Returning Ball object for now. Looking into desired return type.

--- targets.py
+++ targets.py
@@ -1,11 +1,13 @@
 def normFunc(items):
-		item_sum = sum(items)
+		item_sum = float(sum(items))
 		return lambda x : float(x)/item_sum
 
 def cueball_collision_loc(pocket, target):
 		rad = target.rad
 		diam = 2*rad
-		# distance between the balls scaling delta to oen ball 			  # hypotenuse along the impact path
+
+		# distance between the balls scaling delta to oen ball 			  
+		# hypotenuse along the impact path
 		delta_x = pocket.x - target.x
 		delta_y = pocket.y - target.y
 		norm = normFunc([delta_x,delta_y])
@@ -17,22 +19,38 @@ def cueball_collision_loc(pocket, target):
 		# Hacky way of dealing with relative angles
 		# gaurantees the resulting ball is further from the pocket
 
-		if (hit_pos_x - pocket.x) < (target.x - pocket.x):
+		if abs(hit_pos_x - pocket.x) < abs(target.x - pocket.x):
 				hit_pos_x = target.x - x_shift
 						
-		if (hit_pos_y - pocket.y) < (target.y - pocket.y):
+		if abs(hit_pos_y - pocket.y) < abs(target.y - pocket.y):
 				hit_pos_y = target.y - y_shift
 
 		ans = [hit_pos_x, hit_pos_y]
 		print ans	
-		return ans
+		return Ball(hit_pos_x,hit_pos_y)
 
 class Ball:
 		def __init__(self,x,y):
-				self.x = x
-				self.y = y
-				self.rad = 1
+				self.x = float(x)
+				self.y = float(y)
+				self.rad = 1.0
 		
-pocket = Ball(2,4) 
-target = Ball(-3,8) 
-cueball_collision_loc(pocket,target)
+import matplotlib.pyplot as plt
+def debug():
+		pocket = Ball(2,4) 
+		target = Ball(-3,8) 
+		locs = cueball_collision_loc(pocket,target)
+
+		coord = [pocket,target,locs]
+		print map(lambda x:x.x,coord)
+		print map(lambda x:x.y,coord)
+
+
+
+		print "pocket ro, target blue, loc green"
+		plt.plot([coord[0].x],[coord[0].y], 'ro')
+		plt.plot([coord[1].x],[coord[1].y], 'bs')
+		plt.plot([coord[2].x],[coord[2].y], 'g^')
+		plt.axis([-30, 30, -30, 30])
+		plt.show()
+
